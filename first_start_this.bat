0e1a35efc7047f57c3729ae9d84a67470470a70b
LineHaro <lineharo@yandex.ru>
2018-08-04 21:32:43 +0300

    Update first_start_this.bat

--- first_start_this.bat
+++ first_start_this.bat
@@ -9,7 +9,7 @@ call bower install slick-carousel
 call bower install Font-Awesome
 call bower install --save normalize-css
 call bower install html5shiv
-call bower install colorbox
+call bower install fancybox --save
 
 call npm install eslint
 call eslint --init
